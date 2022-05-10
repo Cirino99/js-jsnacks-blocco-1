1c4c56701d412e0755d8c345a0cd9968c0009290
Simone <cirino.simone99@gmail.com>
2022-05-10 15:49:28 +0200

    piccola correzione indice secondo for

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ for(let i=0; i<array1.length; i++){
     const ularray2 = document.createElement('ul');
     for(let j=0; j<array2.length; j++){
         const liarray2 = document.createElement('li');
-        liarray2.append(array2[i]);
+        liarray2.append(array2[j]);
         ularray2.append(liarray2);
     }
     liarray1.append(ularray2);
