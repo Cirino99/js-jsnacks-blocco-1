385e2b7caa422221c04fa12b789ca51f08ec2e33
Simone <cirino.simone99@gmail.com>
2022-05-10 15:47:56 +0200

    finito esercizio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const array1 = ['parola1 array1', 'parola2 array1', 'parola3 array1', 'parola4 array1', 'parola5 array1'];
+const array2 = ['parola1 array2', 'parola2 array2'];
+const myList = document.getElementById('lista');
+for(let i=0; i<array1.length; i++){
+    const liarray1 = document.createElement('li');
+    liarray1.append(array1[i]);
+    const ularray2 = document.createElement('ul');
+    for(let j=0; j<array2.length; j++){
+        const liarray2 = document.createElement('li');
+        liarray2.append(array2[i]);
+        ularray2.append(liarray2);
+    }
+    liarray1.append(ularray2);
+    myList.append(liarray1);
+}
